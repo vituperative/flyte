3ad1eefbc6cef4b9c9b4be228c5f3e183d9c6c72
wipedlife <gigoga367@gmail.com>
2020-02-02 15:54:37 +0700

    comments

--- torrfreedom/include/announcer_class.php
+++ torrfreedom/include/announcer_class.php
@@ -65,22 +65,22 @@ class Announcer {
 	
 };
 
-if (!isset($_GET["info_hash"]))
-        $query = "SELECT $fields FROM torrents ORDER BY info_hash";
-else
-        $query = "SELECT $fields FROM torrents WHERE " . hash_where("info_hash", $_GET["info_hash"]);
-
-$res = mysqli_query($GLOBALS["___mysqli_ston"], $query);
-
-while ($row = mysqli_fetch_assoc($res)) {
-        $r .= "20:" . hash_pad($row["info_hash"]) . "d" .
-                benc_str("complete") . "i" . $row["seeders"] . "e" .
-                benc_str("downloaded") . "i" . $row["times_completed"] . "e" .
-                benc_str("incomplete") . "i" . $row["leechers"] . "e" .
-                "e";
-}
-
-$r .= "ee";
-
-print($r);
-
+//if (!isset($_GET["info_hash"]))
+//        $query = "SELECT $fields FROM torrents ORDER BY info_hash";
+//else
+//        $query = "SELECT $fields FROM torrents WHERE " . hash_where("info_hash", $_GET["info_hash"]);
+//
+//$res = mysqli_query($GLOBALS["___mysqli_ston"], $query);
+//
+//while ($row = mysqli_fetch_assoc($res)) {
+//        $r .= "20:" . hash_pad($row["info_hash"]) . "d" .
+//                benc_str("complete") . "i" . $row["seeders"] . "e" .
+//                benc_str("downloaded") . "i" . $row["times_completed"] . "e" .
+//                benc_str("incomplete") . "i" . $row["leechers"] . "e" .
+//                "e";
+//}
+//
+//$r .= "ee";
+//
+//print($r);
+//
