ba37ff7b72d95cc66c2aabbff9bc914866819473
Vituperative <60452960+vituperative@users.noreply.github.com>
2020-02-03 08:40:44 +0000

    Add div wrapper to siteinfo so it aligns correctly

--- torrfreedom/include/siteinfo.inc.php
+++ torrfreedom/include/siteinfo.inc.php
@@ -1,4 +1,6 @@
+<div id="siteinfo">
 <hr class="separator">
 <b>Tracker Announce:</b> <code>http://nfrjvknwcw47itotkzmk6mdlxmxfxsxhbhlr5ozhlsuavcogv4hq.b32.i2p/a</code>
 <br><b>Contact:</b> <code>torrfreedom@mail.i2p</code>
 <hr class="separator">
+</div>
