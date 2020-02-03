4f88ee1e8219f54a415cad06fce50ec00633ab8c
Vituperative <60452960+vituperative@users.noreply.github.com>
2020-02-03 08:46:15 +0000

    my.php and siteinfo fixes (#12)
    * Fix siteinfo placement
    
    * Buff my.php
    
    * Add div wrapper to siteinfo so it aligns correctly

--- torrfreedom/my.php
+++ torrfreedom/my.php
@@ -9,22 +9,21 @@ loggedinorreturn();
 stdhead($CURUSER["username"] . "'s private page");
 
 if (isset($_GET["edited"])) {
-	print("<h1>Profile updated!</h1>\n");
+	print("<p id=success>Profile updated!</p>\n");
 }
 else
-	print("<h1>Welcome, " . htmlspecialchars($CURUSER["username"]) . "!</h1>\n");
+	print("<p id=success>Welcome, " . htmlspecialchars($CURUSER["username"]) . "!</p>\n");
 
 ?>
-<table border="1" cellpadding="5" cellspacing="0" bordercolor="#667766" align="center">
+<table id=account align=center>
 <tr>
-<td align="center" width="50%"><a href="logout.php" class="biglink">Logout</a></td>
-<td align="center" width="50%"><a href="mytorrents.php" class="biglink">View or edit your torrents</a></td>
+<td colspan=2 align="center"><a href="mytorrents.php" class="biglink">View or edit your torrents</a></td>
 </tr>
 <tr>
 <td colspan="2">
 <form method="post" action="takeprofedit.php">
 <table class="table1" border="1" cellpadding="5" cellspacing="0" bordercolor="#667766" width="100%">
-<tr><td colspan="2" class="heading" align="center">Your profile</td></tr>
+<tr><th colspan="2" class="heading" align="center">Your profile</th></tr>
 <?php
 
 $res = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT COUNT(*) FROM torrents WHERE owner=" . $CURUSER["id"]);
@@ -33,7 +32,7 @@ tr("Uploaded torrents", $row[0]);
 
 $res = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT COUNT(*) FROM comments WHERE user=" . $CURUSER["id"]);
 $row = mysqli_fetch_array($res);
-tr("Written comments", $row[0]);
+tr("Comments posted", $row[0]);
 
 print("<tr><td colspan=\"2\" align=\"center\">Edit your settings</td></tr>\n");
 
