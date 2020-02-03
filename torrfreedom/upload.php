1e124b2d33e35b0a2aef06c616fefc900a09b825
Vituperative <60452960+vituperative@users.noreply.github.com>
2020-02-03 06:15:30 +0000

    Polish upload page

--- torrfreedom/upload.php
+++ torrfreedom/upload.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-require_once("include/bittorrent.inc.php");
+require_once "include/bittorrent.inc.php";
 
 dbconn();
 
@@ -9,10 +9,11 @@ loggedinorreturn();
 stdhead("Upload");
 
 ?>
-<!--
+
 <center>
 <form enctype="multipart/form-data" action="takeupload.php" method="post" accept-charset="utf-8">
 <input type="hidden" name="MAX_FILE_SIZE" value="<?=$max_torrent_size?>" />
+<!--
 <p><h3>The tracker's announce URLs are:</h3>
 <input onclick="this.select();" class="gensmall" readonly size="77" style="
 border: 1px solid white;" value="<?=$tracker_url_key?>/a"><br>
@@ -20,9 +21,10 @@ border: 1px solid white;" value="<?=$tracker_url_key?>/a"><br>
 border: 1px solid white;" value="<?=$tracker_url_key?>/announce"><br>
 <input onclick="this.select();" class="gensmall" readonly size="77" style="
 border: 1px solid white;" value="<?=$tracker_url_key?>/announce.php"></p>
-<p><b>Only upload torrents you're going to seed!<br>Uploaded torrents won't be visible on the main page until you start seeding them.</b></p>
 -->
-<table class="table1" border="2" cellspacing="0" cellpadding="5">
+<p class="note" id="upload">Only upload torrents you're going to seed!<br>Uploaded torrents won't be visible on the main page until you start seeding them.</p>
+
+<table id="uploader" class="table1" border="2" cellspacing="0" cellpadding="5">
 <?php
 
 tr("Upload file", "<input class=\"input\" type=\"file\" name=\"file\" size=\"60\" />\n", 1);
@@ -32,14 +34,15 @@ tr("Description<br />(no html allowed)", "<textarea class=\"input\" name=\"descr
 $s = "<select class=\"input\" name=\"type\">\n<option value=\"0\">(choose one)</option>\n";
 
 $cats = genrelist();
-foreach ($cats as $row)
-	$s .= "<option value=\"" . $row["id"] . "\">" . htmlspecialchars($row["name"]) . "</option>\n";
+foreach ($cats as $row) {
+    $s .= "<option value=\"" . $row["id"] . "\">" . htmlspecialchars($row["name"]) . "</option>\n";
+}
 
 $s .= "</select>\n";
-tr("Type", $s, 1);
+tr("Category", $s, 1);
 
 ?>
-<tr><td align="center" colspan="2"><input class="input" type="submit" value="Do it!" /></td></tr>
+<tr><td align="center" colspan="2"><input class="input" type="submit" value="Upload Torrent" /></td></tr>
 </table>
 </form>
 </center>
