7380e9451624d6e2db2b06af97b25fea80138fb1
Vituperative <60452960+vituperative@users.noreply.github.com>
2020-02-04 09:20:33 +0000

    Delete siteinfo.inc.php

--- siteinfo.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "include/bittorrent.inc.php";
-dbconn(0);
-?>
-<div id="siteinfo">
-<b>Tracker Announce:</b> <code><?php echo "$tracker_url_key" ?>/a</code>
-<hr class="separator">
-</div>
