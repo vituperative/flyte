55bdb16e4a333925862a6d5f33788962032bd40c
torrfreedom <60451687+torrfreedom@users.noreply.github.com>
2020-02-04 22:06:03 +0200

    Fix deleting torrent

--- torrfreedom/edit.php
+++ torrfreedom/edit.php
@@ -51,9 +51,7 @@ if (!isset($CURUSER) || ($CURUSER["id"] != $row["owner"] && $CURUSER["admin"] !=
         tr("Banned", "<input type=\"checkbox\" name=\"banned\"" . (($row["banned"] == "yes") ? " checked=\"checked\"" : "") . " value=\"1\" /> Banned", 1);
     }
 
-    print("<tr id=dostuff><td colspan=\"2\" align=\"center\"><input type=\"submit\" value=\"Update Torrent\" /> ");
-    print("<input type=\"submit\" value=\"Delete Torrent\" />\n");
-    print("<input type=\"checkbox\" name=\"sure\" value=\"1\" /> Confirm delete</td></tr>\n");
+    print("<tr id=dostuff><td colspan=\"2\" align=\"center\"><input type=\"submit\" value=\"Update Torrent\" /> </td></tr>");
     print("</table>\n");
     print("</form>\n");
     print("<hr />\n");
@@ -63,7 +61,8 @@ if (!isset($CURUSER) || ($CURUSER["id"] != $row["owner"] && $CURUSER["admin"] !=
     if (isset($_GET["returnto"])) {
         print("<input type=\"hidden\" name=\"returnto\" value=\"" . htmlspecialchars($_GET["returnto"]) . "\" />\n");
     }
-
+    print("<input type=\"submit\" value=\"Delete Torrent\" />\n");
+    print("<input type=\"checkbox\" name=\"sure\" value=\"1\" /> Confirm delete\n");
     print("</form>\n");
     print("</p>\n");
 }
