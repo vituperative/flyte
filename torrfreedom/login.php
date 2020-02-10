b73bd56da91da9fc82d52a199109991a1b668b73
Vituperative <60452960+vituperative@users.noreply.github.com>
2020-02-10 12:17:27 +0000

    Capitalize tracker name on Login page panel

--- torrfreedom/login.php
+++ torrfreedom/login.php
@@ -14,7 +14,7 @@ if (!empty($_GET["returnto"])) {
 <p id=toast class=warn>Please ensure cookies are enabled in your browser.</p>
 <div class="tablewrap slim">
 <table id=dologin>
-<tr><th colspan=2>Login to <?php echo "$tracker_title"; ?></th></tr>
+<tr><th colspan=2>Login to <?php $sitename = ucwords(strtolower($tracker_title)); echo $sitename; ?></th></tr>
 <tr><td>Username</td><td><input id=username class=input type=text size=20 name=username /></td></tr>
 <tr><td>Password</td><td><input class=input type=password size=20 name=password /></td></tr>
 <tr id=dostuff><td colspan=2><input type=submit value="Log in!" class=input /></td></tr>
