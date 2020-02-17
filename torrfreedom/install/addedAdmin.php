97b7015693aeacf31dba3b6574fa5c0c390376c8
Vituperative <60452960+vituperative@users.noreply.github.com>
2020-02-17 21:33:26 +0000

    Move installer.css loading to page_header.inc.php

--- torrfreedom/install/addedAdmin.php
+++ torrfreedom/install/addedAdmin.php
@@ -3,7 +3,6 @@ require_once "install_class.php";
 require_once "../include/bittorrent.inc.php";
 require_once "../include/page_header.inc.php";
 $installer = new Installer();
-print("<link rel=stylesheet href=installer.css type=text/css>");
 ?>
 <table id=wrapper>
     <tr>
