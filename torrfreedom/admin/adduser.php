eba45fc988a6b46993deed8bd2630f7a84dc3054
potatowipedlifereverse <potatolivingstonei2p@gmail.com>
2020-02-20 00:34:29 +0300

    adduser - password

--- torrfreedom/admin/adduser.php
+++ torrfreedom/admin/adduser.php
@@ -5,12 +5,13 @@ $admin = new admin();
 print("<div id=server class=users>\n");
 
 if(isset($_GET['add_user'])){
-	$admin->addUser($_GET['add_user'], "123456", $_GET['admin']);
+	$admin->addUser($_GET['add_user'], $_GET['password'], $_GET['admin']);
 }
 
 ?>
 <form action=adduser.php style="position:absolute" method=GET>
 	Nick <input type=text placeholder=nick name=add_user>
+	password <input type=text placeholder=pass name=password>
 	admin(yes/no) <input type=text name=admin value='no'>
 	<input type=submit>
 	<hr/>
