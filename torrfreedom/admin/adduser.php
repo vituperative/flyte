c738a02bce80dd9bf04eae3e5b231aff2f4540dc
potatowipedlifereverse <potatolivingstonei2p@gmail.com>
2020-02-21 00:34:30 +0300

    admin panel updates

--- torrfreedom/admin/adduser.php
+++ torrfreedom/admin/adduser.php
@@ -2,21 +2,28 @@
 require 'admin_class.php';
 $admin = new admin();
 
-print("<div id=server class=users>\n");
-
 if(isset($_GET['add_user'])){
-	$admin->addUser($_GET['add_user'], $_GET['password'], $_GET['admin']);
+	$ret=$admin->addUser($_GET['add_user'], $_GET['password'], $_GET['admin']);
+	if( $ret !== TRUE){
+		print ("<div style='text-transform:uppercase'>Some is wrong: ".$ret."</div>");
+	}
+	else header("Location: users.php");
 }
 
 ?>
-<form action=adduser.php style="position:absolute" method=GET>
-	Nick <input type=text placeholder=nick name=add_user>
-	password <input type=text placeholder=pass name=password>
-	admin(yes/no) <input type=text name=admin value='no'>
-	<input type=submit>
-	<hr/>
+<!-- Some new DROZD code -->
+<form action=adduser.php method=GET>
+<div id=server class=usermanage>
+<table>
+<tr><th colspan=2>Create User Account</th></tr>
+<tr><td>Username</td><td><input type=text name=add_user required></td></tr>
+<tr><td>Password</td><td><input type=text name=password required></td></tr>
+<tr><td>	Admin Privileges</td><td>
+<select name='admin'>
+  <option value=no selected>No&hellip; just a regular user account</option>
+  <option value=yes>Yes&hellip; full administrative privileges</option></td></tr>
+<tr id=dostuff><td colspan=2>	<input type=submit value="Add User"</td></tr>
+</table>
+</div>
 </form>
-<?php
-
-stdfoot();
-?>
+<?php stdfoot(); ?>
--- torrfreedom/admin/deluser.php
+++ torrfreedom/admin/deluser.php
@@ -2,20 +2,24 @@
 require 'admin_class.php';
 $admin = new admin();
 
-print("<div id=server class=users>\n");
-
 if(isset($_GET['del_user'])){
-	print("DEL!");
-	$admin->delUserByUsername($_GET['del_user']);
+	$ret=$admin->delUserByUsername($_GET['del_user']);
+	if( !$ret ){
+		print ("<div style='text-transform:uppercase'>Some is wrong: ".$admin->getLastSQLError()."</div>");
+	}
+	header("Location: users.php");
 }
 
 ?>
-<form action=deluser.php style="position:absolute" method=GET>
-	Nick <input type=text placeholder=nick name=del_user>
-	<input type=submit>
-	<hr/>
+<form action=deluser.php method=GET>
+<div id=server class=usermanage>
+<table>
+<tr><th colspan=2>Delete User Account</th></tr>
+<tr><td>Username</td><td><input type=text name=del_user required></td></tr>
+<tr><td>Delete Torrents</td><td><label><input type=checkbox name=del_torrents>&nbsp; Delete all torrents uploaded by the user</label></td></tr>
+<tr><td>Delete Comments</td><td><label><input type=checkbox name=del_comments>&nbsp; Delete all comments posted by the user</label></td></tr>
+<tr id=dostuff><td colspan=2>	<input type=submit value="Delete User"</td></tr>
+</table>
+</div>
 </form>
-<?php
-
-stdfoot();
-?>
+<?php stdfoot(); ?>
