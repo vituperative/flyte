3f92b0643ab20bd687d9071143affd1ba63c5529
chris <z3d@i2pmail.org>
2020-04-15 19:01:42 +0000

    Admin: More torrent.php cleanups

--- torrfreedom/admin/torrents.php
+++ torrfreedom/admin/torrents.php
@@ -17,21 +17,26 @@ if (!isset($_GET['user'])) {
     $result = $admin->getTorrentsByUserNick($_GET['user'], $offset);
     $username = "Torrents owned by: " . $_GET['user'];
 }
+?>
 
-echo "<style type=text/css>body {min-width: 1000px !important;}</style>";
-echo "<div id=server class=torrents>\n<table>\n<tr>";
-echo "<th>Type</th>";
-echo "<th>$username</th>";
-echo "<th>Seed</th>";
-echo "<th>Leech</th>";
-echo "<th>DL's</th>";
-echo "<th>Views</th>";
-echo "<th>Cmts</th>";
-echo "<th>Added</th>";
-echo "<th>Visible</th>";
-echo "<th>Banned</th>";
-echo "<th>Nuke</th></tr>";
+<style type=text/css>body {min-width: 1000px !important;}</style>
+<div id=server class=torrents>
+<table>
+<tr>
+<th>Type</th>
+<th><?=$username?></th>
+<th>Seed</th>
+<th>Leech</th>
+<th>DL's</th>
+<th>Views</th>
+<th>Cmts</th>
+<th>Added</th>
+<th>Visible</th>
+<th>Banned</th>
+<th>Nuke</th>
+</tr>
 
+<?php
 function delTorrent($torid, $name, $user)
 {
     printf("<td><a href=\"delTorrent.php?wdel_id='%s'&amp;name='%s'&amp;user='%s'\" class=button><span class=no></span></a></td>", $torid, $name, $user);
@@ -40,6 +45,7 @@ function delTorrent($torid, $name, $user)
 while ($row = mysqli_fetch_array($result)) {
     echo "<tr>";
     $torid = $row['id'];
+    $form = "<form action=modifytorrent.php method=GET><input type=submit value=Apply><input type=hidden name=torid value=$torid />";
 
     echo "<td>";
     if (isset($row["category"])) {
@@ -57,12 +63,11 @@ while ($row = mysqli_fetch_array($result)) {
     echo "<td>" . $row['views'] .  "</td>";
     echo "<td>" . $row['comments'] .  "</td>";
     echo "<td>" .  preg_replace("/ .*/", "", $row["added"]) .  "</td>";
-    echo "<td><form action=modifytorrent.php method=GET><input type=checkbox name=visible><input type=submit value=Apply><input type=hidden name=torid value=$torid /><input type=hidden name=do value=visible /></form></td>"; // TODO apply in situ, not via vistorrent.php
-    echo "<td>
-<form action=modifytorrent.php method=GET><input type=checkbox name=banned><input type=submit value=Apply><input type=hidden name=torid value=$torid /><input type=hidden name=do value=banned /></form></td>"; // TODO apply in situ, not via bantorrent.php
+    echo "<td>$form<input type=checkbox name=visible><input type=hidden name=do value=visible /></form></td>"; // TODO apply in situ, not via vistorrent.php
+    echo "<td>$form<input type=checkbox name=banned><input type=hidden name=do value=banned /></form></td>"; // TODO apply in situ, not via bantorrent.php
     delTorrent($torid, $row['name'], $_GET['user']);
     echo "</tr>\n";
 }
 
-echo "</table></div>";
+echo "</table>\n</div>";
 stdfoot();
