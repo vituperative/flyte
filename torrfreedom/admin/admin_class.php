49bb4450d22c7e9cc051f313ea8fcd8317d32396
potatowipedlifereverse <potatolivingstonei2p@gmail.com>
2020-04-19 18:30:11 +0300

    categories stuff

--- torrfreedom/admin/admin_class.php
+++ torrfreedom/admin/admin_class.php
@@ -33,13 +33,27 @@ class sql
         "getCountOfTBWhereMoreFunc" => "SELECT COUNT(*) FROM %s WHERE %s > %s",
         "isAdmin" => "SELECT * FROM users WHERE username='%s' AND admin='yes';",
         "getAllTorrents" => "SELECT * FROM torrents LIMIT %d OFFSET %d",
-        "getNameOfCategoryByID" => "SELECT * FROM categories WHERE id='%d'"
+        "getNameOfCategoryByID" => "SELECT * FROM categories WHERE id='%d'",
+	"getCategoryByName"=> "select * from categories where name='%s'",
+	"getCategoryByID"=> "select * from categories where id='%d'",
+	"addCategoryByName" => "INSERT INTO categories(name,sort_index) values(%s,LAST_INSERT_ID()*10);",
+	"delCategoryByName"=> "DELETE FROM categories where name='%s';",
+	"delCategoryByID"=> "DELETE FROM categories where id='%d';",
+	"changeCategoryNameByID" => "update categories set name='%s' where id='%d';",
+	"changeCategorySortIndexByID"=> "update categories set sort_index='%d' where id='%d';",
+	"fixAutoIncrementToTable" => "ALTER TABLE %s AUTO_INCREMENT='%d'"
+/*
+
+*/
     );
     function doSQL($sprintf, ...$arguments)
     {
         $string = vsprintf($sprintf, $arguments);
         //printf("Debug info: %s\n\r<br>", $string);
         return $result = mysqli_query($this->con, $string);
+    }
+    function fixAutoIncrementToTable($table, $v){
+	return $this->doSQL( sql::sqls['fixAutoIncrementToTable'], $table, $v );
     }
     function getSQLCon()
     {
@@ -217,17 +231,41 @@ class torrents extends comments
 
 class categories extends torrents
 {
-    function countCategories()
-    {
-        return $this->getCountOfTB("categories");
-    }
-    function getNameOfCategoryByID($id)
-    {
-        //getNameOfCategoryByID
-        $category = $this->doSQL(sql::sqls['getNameOfCategoryByID'], $id);
-        //$category=mysqli_fetch_array($category)['name'];
-        return mysqli_fetch_array($category)['name'];
-    }
+	function countCategories(){
+		return $this->getCountOfTB("categories");
+	}
+	function getCategoryByID($id){ // id, name, sort_index
+		$category=$this->doSQL( sql::sqls['getCategoryByID'], $id );
+		return mysqli_fetch_array($category);
+	}
+	function getNameOfCategoryByID($id){
+		//getNameOfCategoryByID
+		//$category=$this->doSQL( sql::sqls['getNameOfCategoryByID'], $id );
+		//$category=mysqli_fetch_array($category)['name'];
+		//return mysqli_fetch_array($category)['name'];
+		return $this->getCategoryByID($id)['name'];
+	}
+	function getCategoryByName($name){
+		$category=$this->doSQL( sql::sqls['getCategoryByName'], $name );
+		return mysqli_fetch_array($category);
+	}
+	function addCategoryByName($name){
+		return $this->doSQL( sql::sqls['addCategoryByName'], $name );
+	}
+	function delCategoryByName($name){
+		return $this->doSQL( sql::sqls['delCategoryByName'], $name );
+	}
+	function delCategoryByID($id){
+		return $this->doSQL( sql::sqls['delCategoryByID'], $id );
+	}
+	function changeCategoryNameByID($id,$name){
+		//  "changeCategoryNameByID" => "update categories set name='%s' where id='%d';",
+		return $this->doSQL( sql::sqls['changeCategoryNameByID'], $name, $id );
+	}
+	function changeCategorySortIndexByID($id, $sort){
+		//"changeCategorySortIndexByID"=> "update categories set sort_index='%d' where id='%d';",
+		return $this->doSQL( sql::sqls['changeCategorySortIndexByID'], $sort, $id );
+	}
 }
 
 class peers extends categories
