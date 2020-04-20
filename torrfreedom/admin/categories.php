1258f2ed9d36113fd41c2aacd61ff10797652b44
chris <z3d@i2pmail.org>
2020-04-20 01:12:56 +0000

    admin/categories.php: presentation

--- torrfreedom/admin/categories.php
+++ torrfreedom/admin/categories.php
@@ -1,17 +1,19 @@
 <?php
+global $tracker_path;
 require 'admin_class.php';
 $admin = new admin();
-$categoires = $admin->getCategories();
-echo "<div id=server class=categories>\n<table>
-<tr><th>id</th><th>name</th><th>sort_index</th><th>delete</th></tr>\n";
-while($row = mysqli_fetch_array($categoires)){
-	echo "<tr>";
-	echo "<td>" . $row['id'] . "</td>";
-	echo "<td>" . $row['name'] . "</td>";
-	echo "<td>" . $row['sort_index'] . "</td>";
-	echo "<td><a href='delCtagory.php?wdel_category=".$row['id']."' class=button><span class=no></span></a></td>";
-	echo "</tr>";
+$categories = $admin->getCategories();
+//$torrentcount = $admin->getCountOfTorrentsInCategory();
+echo "<div id=server class=categories><table>
+<tr><th></th><th>Name (torrents)</th><!--<th>ID</th><th>Sort Index</th>--><th>Delete</th></tr>";
+while ($row = mysqli_fetch_array($categories)) {
+    echo "<tr>";
+    echo "<td><a href=" . $tracker_path . "?cat=" . $row['id'] . "\" class=\"catlink\" title=\"ID: " . $row['id'] . " / Sort Index: " . $row['sort_index'] . "\"><img src=\"" . $tracker_path . "pic/" . $row['id'] . ".png\" width=24 height=24></a></td>";
+    echo "<td>" . $row['name'] . " (" . $torrentcount . ")</td>";
+    echo "<td><a href='delCategory.php?wdel_category=" . $row['id'] . "' class=button><span class=no></span></a></td>";
+    echo "</tr>";
 }
-
+echo "</table></div>";
 ?>
+
 <?php stdfoot(); ?>
