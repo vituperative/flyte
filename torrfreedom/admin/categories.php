3a21cb8ebc14cbe5d2c6f454b6477310fd5f5179
chris <z3d@i2pmail.org>
2020-04-23 14:43:24 +0000

    categories.php presentational tweaks

--- torrfreedom/admin/categories.php
+++ torrfreedom/admin/categories.php
@@ -5,13 +5,13 @@ $admin = new admin();
 $categories = $admin->getCategories();
 //$torrentcount = $admin->getCountOfTorrentsInCategory();
 echo "<div id=server class=categories><table>";
+echo "<tr><th colspan=3>Category Management</th></tr>";
 echo "<tr id=dostuff><td colspan=4><input class=create type=text name=\"\" value=\"\" placeholder=\"category name\"><input type=submit value=Create></td></tr>";
-echo "<tr><th></th><th>Category Name</th><!--<th>ID</th><th>Sort Index</th>--><th>Nuke</th></tr>";
 while ($row = mysqli_fetch_array($categories)) {
     echo "<tr>";
     echo "<td><a href=\"" . $tracker_path . "?cat=" . $row['id'] . "\" class=catlink title=\"ID: " . $row['id'] . " / Sort Index: " . $row['sort_index'] . "\"><img src=\"" . $tracker_path . "pic/" . $row['id'] . ".png\" width=24 height=24></a></td>";
     echo "<td><input type=text name=\"" . $row['id'] . "\" value=\"" . $row['name'] . "\" disabled></td>";
-    echo "<td><a href='delCategory.php?wdel_category=" . $row['id'] . "' class=button><span class=no></span></a></td>";
+    echo "<td title=\"Delete " . $row['name'] . " category\"><a href='delCategory.php?wdel_category=" . $row['id'] . "' class=button><span class=no></span></a></td>";
     echo "</tr>";
 }
 echo "<tr id=dostuff><td colspan=3><input type=submit value=\"Sort Alphabetically\"><input type=reset value=\"Cancel\"><input type=submit value=\"Save Changes\"></td></tr>";
